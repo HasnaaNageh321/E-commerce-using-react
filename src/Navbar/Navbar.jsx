dfbcbc252ee2cd864b6376a383342c236a1c9013
HasnaaNageh123 <164903316+HasnaaNageh123@users.noreply.github.com>
2024-08-07 14:38:29 +0300

    first commit

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbarstyle.css';
 
 function Navbar() {
-  
-  
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
       <div className="navconst">
         <div className="contentHeader">
           <div className="logo">
-            <Link to="/"> {/* Use Link component for internal navigation */}
-              e-commerce
-            </Link>
-            <i className="fas fa-shopping-cart"></i> {/* Corrected class to className */}
+            <p>e-commerce</p>
+            <i className="fas fa-shopping-cart"></i>
           </div>
           <nav>
-            <ul >
-              <li><Link to="/">Home</Link></li> {/* Use Link for internal navigation */}
-              <li><Link to="/products">Products</Link></li>
-              <li><Link to="/gallery">Gallery</Link></li>
-              <li><Link to="/contact">Contact us</Link></li>
+            <ul className={menuOpen ? 'show' : ''}>
+              <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+              <li><Link to="/products" onClick={() => setMenuOpen(false)}>Products</Link></li>
+              <li><Link to="/gallery" onClick={() => setMenuOpen(false)}>Gallery</Link></li>
+              <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact us</Link></li>
             </ul>
+            <div className="menu-icon" onClick={toggleMenu}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black">
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+              </svg>
+            </div>
           </nav>
         </div>
       </div>
